7eb66587ad79abfc885ba5edbee51c40bf498482
praveensinghal2010 <praveen.singhal2010@gmail.com>
2025-10-18 21:25:42 +0530

    added

--- src/test/java/StepDefinitions/MyStepdefs.java
+++ src/test/java/StepDefinitions/MyStepdefs.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class MyStepdefs {
 
@@ -14,7 +15,21 @@ public class MyStepdefs {
     public void userIsAbleToOpenHomepage() throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
-         driver= new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Check if running on AWS CodeBuild
+
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--disable-web-security");
+
+        driver= new ChromeDriver(options);
+
         driver.get("https://www.google.com");
         Thread.sleep(5000);
 
